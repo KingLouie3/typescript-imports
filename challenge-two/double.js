85a5e00aa246bce9ea84b9a2f0da1338ae0b0fc4
Louis Sutton <louis.sutton.iii@gmail.com>
2019-11-06 19:41:59 -0800

    Passed

---
+++ challenge-two/double.js
@@ -0,0 +1,18 @@
+"use strict";
+// ##### Instructions
+// * Create  two files named index.ts and double.ts
+// * In double.ts create a function named **double** that takes in a number and returns it doubled
+// * Export the function double
+// * In index.ts import the function double from double.ts
+// * invoke the function in index.ts, with an input of **15** and store the value in an exported variable named **result**
+// * Convert the code to javascript by running the following command
+// ```
+// $ tsc index.ts
+// ```
+// * Now test your code with:
+// ```
+// $ npm test -- challenge-two
+exports.__esModule = true;
+exports.double = function (number) {
+    return number * 2;
+};
---
+++ challenge-two/index.ts
@@ -0,0 +1,17 @@
+// ##### Instructions
+// * Create  two files named index.ts and double.ts
+// * In double.ts create a function named **double** that takes in a number and returns it doubled
+// * Export the function double
+// * In index.ts import the function double from double.ts
+// * invoke the function in index.ts, with an input of **15** and store the value in an exported variable named **result**
+// * Convert the code to javascript by running the following command
+// ```
+// $ tsc index.ts
+// ```
+// * Now test your code with:
+// ```
+// $ npm test -- challenge-two
+
+import {double} from "./double";
+
+export const result = double(15);
